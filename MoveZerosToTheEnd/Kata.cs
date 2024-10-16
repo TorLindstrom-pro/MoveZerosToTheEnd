c8a07574ed8d8674a2aae88d47c1cb4683a7d367
TorLindstrom <tor.lindstrom@winlas.se>
2024-10-16 09:49:09 +0200

    kata: move zeros to the end

---
+++ MoveZerosToTheEnd/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace MoveZerosToTheEnd;
+
+public class Kata
+{
+	public static int[] MoveZeroes(int[] arr)
+	{
+		return arr.OrderBy(i => i == 0 ? 2 : 1).ToArray();
+	}
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,15 +1,45 @@
+using MoveZerosToTheEnd;
+
 namespace Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+	[SetUp]
+	public void Setup()
+	{
+	}
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+	[Theory]
+	[TestCase(0)]
+	[TestCase(1)]
+	public void SingleDigit_IsReturnedAsIs(int digit)
+	{
+		var result = Kata.MoveZeroes([digit]);
+		var expectedResult = new[] { digit };
+		Assert.That(result, Is.EquivalentTo(expectedResult));
+	}
+
+	[Test]
+	public void InputWithOnlyNonZeroDigits_IsReturnedAsIs()
+	{
+		var result = Kata.MoveZeroes([1, 2, 3]);
+		var expectedResult = new[] { 1, 2, 3 };
+		Assert.That(result, Is.EquivalentTo(expectedResult));
+	}
+
+	[Test]
+	public void SingleZero_MovesToTheEnd()
+	{
+		var result = Kata.MoveZeroes([0, 1, 2, 3]);
+		var expectedResult = new[] { 1, 2, 3, 0 };
+		Assert.That(result, Is.EquivalentTo(expectedResult));
+	}
+
+	[Test]
+	public void AllZeros_MovesToTheEnd()
+	{
+		var result = Kata.MoveZeroes([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]);
+		var expectedResult = new[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 };
+		Assert.That(result, Is.EquivalentTo(expectedResult));
+	}
 }
